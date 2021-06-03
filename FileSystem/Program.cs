76b2ab910c7823c0bc6b508f145025fb7b859b38
Alican Demirtas <demirtasdev@hotmail.com>
2021-06-03 14:42:43 +0100

    Folders: Creating a New Folder

---
+++ FileSystem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace FileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateDirectory();
+            Console.Read();
+        }
+
+        private static void CreateDirectory()
+        {
+            var dirName = "TestFolder";
+
+            if (Directory.Exists(dirName))
+                Console.WriteLine($"Directory '{dirName}' already exists.");
+            else
+            {
+                Directory.CreateDirectory(dirName);
+                Console.WriteLine($"Directory '{dirName}' is created.");
+            }
+        }
+    }
+}
