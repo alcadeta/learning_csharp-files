31ec20939bd7fb7535132d30fed3411989936a8f
Alican Demirtas <demirtasdev@hotmail.com>
2021-06-04 11:04:42 +0100

    Files: Copy, Edit, and Move the Config File

--- FileSystem/Program.cs
+++ FileSystem/Program.cs
@@ -13,6 +13,7 @@ namespace FileSystem
             ReadConfig();
             CreateDirectory();
             CreateFile();
+            ArchiveConfig();
             MoveDataIntoArchive();
             DeleteTemp();
         }
@@ -20,15 +21,17 @@ namespace FileSystem
         /// Config path
         private const string CONFIG_FILE = "Config.txt";
 
-        // Folder paths
+        /// Folder names
         private enum FolderName { Workspace, Archive, Temp, SavedData }
         private static string[] _folders =
         {
-            @"Workspace",
-            @"Workspace/Archive",
-            @"Workspace/Temp",
-            @"Workspace/Temp/SavedData"
+            @"Workspace/",
+            @"Workspace/Archive/",
+            @"Workspace/Temp/",
+            @"Workspace/Temp/SavedData/"
         };
+        private static string GetFolderByName(FolderName folderName) =>
+            _folders[(int) folderName];
 
         /// Create directories the paths of which are specified in _Folders
         private static void CreateDirectory()
@@ -36,11 +39,12 @@ namespace FileSystem
             var total = _folders.Length;
             for (var i = 0; i < total; i++)
             {
-                var dirName = _folders[i];
+                var dirName = GetFolderByName((FolderName) i);
 
                 if (Directory.Exists(dirName))
                 {
                     Console.WriteLine($"Directory '{dirName}' already exists.");
+
                 }
                 else
                 {
@@ -53,7 +57,7 @@ namespace FileSystem
         /// Delete the `Workspace/Temp` folder recursively
         private static void DeleteTemp()
         {
-            var tempDir = _folders[(int) FolderName.Temp];
+            var tempDir = GetFolderByName(FolderName.Temp);
             if (Directory.Exists(tempDir)) // Determine whether the folder exists
                 Directory.Delete(tempDir, true); // Delete recursively if it does
         }
@@ -61,17 +65,20 @@ namespace FileSystem
         /// Move the SavedData directory into archive
         private static void MoveDataIntoArchive()
         {
-            var dataDir = _folders[(int) FolderName.SavedData];
-            if (Directory.Exists(dataDir))
-                Directory.Move(dataDir, _folders[(int) FolderName.Archive] +
-                                        $"/{Path.GetFileName(dataDir)}" +
-                                        $"_{DateTime.Now:yyyyMMddHHmmss}");
+            var dataDir = GetFolderByName(FolderName.SavedData);
+
+            if (!Directory.Exists(dataDir)) return;
+
+            var archivedDataDir = GetFolderByName(FolderName.Archive) +
+                                  $"{Path.GetFileName(dataDir)}" +
+                                  $"_{DateTime.Now:yyyyMMddHHmmss}";
+            Directory.Move(dataDir, archivedDataDir);
         }
 
         /// Create a new file in SavedData, and write some string to it
         private static void CreateFile()
         {
-            var path = $"{_folders[(int) FolderName.SavedData]}/TestFile.txt";
+            var path = $"{GetFolderByName(FolderName.SavedData)}TestFile.txt";
 
             File.WriteAllText(path, "Hello world!");
 
@@ -80,8 +87,8 @@ namespace FileSystem
             var extension = fileInfo.Extension;
             var size = fileInfo.Length;
 
-            Console.WriteLine($"Created the file '{name}' with the extension of " +
-                              $"'{extension}' and the size of {size} bytes.");
+            Console.WriteLine($"Created the file '{name}' with the extension " +
+                              $"of '{extension}' and the size of {size} bytes.");
         }
 
         /// Create a config file that contains all folder names, one per line
@@ -91,6 +98,7 @@ namespace FileSystem
                 File.WriteAllLines(CONFIG_FILE, _folders);
         }
 
+        /// Read the config file and overwrite folders array with each line
         private static void ReadConfig()
         {
             var lines = File.ReadAllLines(CONFIG_FILE);
@@ -100,12 +108,28 @@ namespace FileSystem
             // there are elements in the folders array.
             Array.Resize(ref _folders, total);
 
-            for (var i = 0; i < total; i++)
-            {
-                var path = lines[i];
-                Console.WriteLine($"Setting path — {path}");
-                _folders[i] = path;
-            }
+            for (var i = 0; i < total; i++) _folders[i] = lines[i];
+        }
+
+        private static void ArchiveConfig()
+        {
+            var configPath = CONFIG_FILE;
+            var configName = Path.GetFileName(configPath);
+            var tempPath = $"{GetFolderByName(FolderName.Temp)}{configName}";
+            var newPath = $"{GetFolderByName(FolderName.SavedData)}{configName}";
+
+            File.Copy(configPath, tempPath);
+
+            var configLines = File.ReadAllLines(tempPath);
+            var configString = string.Join(Environment.NewLine, configLines);
+            var workspaceDirName = Path.GetDirectoryName(GetFolderByName(FolderName.Workspace));
+            var newWorkspaceDirName = $"{workspaceDirName}{DateTime.Now:yyyyMMddHHmmss}";
+
+            configString = configString.Replace(workspaceDirName ?? string.Empty,
+                                                newWorkspaceDirName);
+
+            File.WriteAllText(tempPath, configString);
+            File.Move(tempPath, newPath);
         }
     }
 }
